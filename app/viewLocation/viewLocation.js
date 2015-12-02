cd10c9c0ce3a5da1b7dea3c39e9da9bc27dc9b93
Juan Tabares <juan.ctt@live.com>
2015-12-03 00:29:39 +0900

    put data in separate file

--- app/viewLocation/viewLocation.js
+++ app/viewLocation/viewLocation.js
@@ -9,21 +9,16 @@ angular.module('tripito.viewLocation', ['ngRoute'])
   });
 }])
 
-.controller('ViewLocation1Ctrl', ['$scope', function($scope) {
+.controller('ViewLocation1Ctrl', ['$scope', '$http', function($scope, $http) {
   $scope.header = {
     name: {name: "Name", sortable:'true', width: '20%'},
     address: {name: "Address", sortable:'true', width: ''},
     owner: {name: "Owner", sortable:'true', width: '20%'}
   };
 
-  $scope.locations = [
-    {name: '大文字山', address: '〒606-0001 京都府京都市左京区', owner: 'Juan Tabares'},
-    {name: '四天王寺', address: '〒543-0051 大阪府大阪市天王寺区四天王寺1-11-18', owner: 'Juan Tabares'},
-    {name: '清水寺', address: '〒605-0862 京都府京都市東山区清水一丁目294', owner: 'Juan Tabares'},
-    {name: '三柱神社', address: '〒832-0826 福岡県柳川市三橋町高畑323-1', owner: 'Juan Tabares'},
-    {name: '三柱神社', address: '大分県別府市小倉6', owner: 'Juan Tabares'},
-  ];
-
+  $http.get('viewLocation/data.json').then(function(res){
+    $scope.locations = res.data;
+  });
 
   $scope.sort = {
     column: 'name',
